230c66804cbc79949addfaa1258cdbe3ada26b9c
danee593 <danee593@gmail.com>
2024-03-06 15:16:14 +0000

    minor changes

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from controller.main import Controller
 from view.main import View
+import logging
 
 if __name__ == "__main__":
+    # TODO: create the logging hierarchy and pass it to the modules.
     try:
         view = View()
         controller = Controller(view)
