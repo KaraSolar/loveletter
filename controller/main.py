17f47c6fcbce8c25a914c042eedd12434b5952da
danee593 <danee593@gmail.com>
2024-03-27 12:37:28 +0000

    added workers.py file

--- controller/main.py
+++ controller/main.py
@@ -16,17 +16,17 @@ make them non-daemonic and use a suitable signalling mechanism such as an Event.
 
 import threading
 import queue
-from model.modbus_query import ModbusQuery
-from pymodbus.exceptions import ModbusException, ModbusIOException
-from model.telemetry_database import TelemetryDatabase
 from .view_controller import ViewController
+from workers import WorkerDatabase, WorkerModbus
 
 class Controller:
     def __init__(self, view):
         self.view = view
-        self.modbus_query_worker_destroy_event = threading.Event()
-        self.trip_start_signal_event = threading.Event()
-        self.data_base_queue = queue.Queue(maxsize=0)
+        self.stop_workers_signal = threading.Event()
+        self.trip_mode_flag_signal = threading.Event()
+        self.queue_worker_database = queue.Queue(maxsize=0)
+        self.queue_main_thread = queue.Queue(maxsize=0)
+        self.lock = threading.Lock()
 
         self.view_controller = ViewController(self.view, self.data_base_queue, self.registers_lock,
                                               self.data_bus, self.trip_start_signal_event)
@@ -40,7 +40,6 @@ class Controller:
 
     def closing_keys(self):
         self.view.root.bind('<Escape>', self.close_on_escape)
-        self.view.root.bind('<Control-c>', self.close_on_escape)
 
     def close_on_escape(self,event=None):
         print("cerrando el programa...")
@@ -69,43 +68,3 @@ class Controller:
         self.view_controller.update_view()
         self.view.root.after(1000, self.loop_update_view)
 
-    def telemetry_database_worker(self):
-        telemetry_database = TelemetryDatabase()
-        while True:  # if queue is large this will take time to complete
-            # the choice of true and that there is a explicit message in the queue "destroy"
-            # is because if the queue has more elements the objective is to actually write them
-            # to the db instead of closing the program when queue is not empty
-            message = self.data_base_queue.get()
-            if message == "destroy":
-                break
-            elif message == "telemetry":
-                with self.registers_lock:
-                    telemetry_database.insert_telemetry(self.data_bus["telemetry"])
-            elif message == "end_of_trip":
-                telemetry_database.end_of_trip()
-            else:
-                telemetry_database.insert_trip(message)
-        telemetry_database.close_connection()
-
-    def modbus_query_worker(self):
-        modbus_query = ModbusQuery()
-        while self.modbus_query_worker_destroy_event.is_set() is False:  # replace with close event
-            with self.registers_lock: # could be eliminated
-                rate = self.data_bus["sampling_rate"] # stays
-            try:
-                modbus_query.read_telemetry_registers()
-            except (ModbusIOException, ModbusException, AttributeError) as exe:
-                # add to logs
-                with self.registers_lock:
-                    for key in self.data_bus["telemetry"]:
-                        self.data_bus["telemetry"][key] = None
-                continue
-            else:
-                with self.registers_lock:
-                    for index, key in enumerate(self.data_bus["telemetry"]):
-                        self.data_bus["telemetry"][key] = modbus_query.register_values[index]
-                self.data_base_queue.put("telemetry")
-            if self.trip_start_signal_event.wait(timeout=rate): # wait rate unless trip_start_signal received
-                self.trip_start_signal_event.clear()
-                continue
-        modbus_query.disconnect()
--- controller/workers.py
+++ controller/workers.py
@@ -1,11 +1,60 @@
+import queue
 import threading
+import ttkbootstrap
+import time
+from model.modbus_query import ModbusQuery
+from model.telemetry_database import TelemetryDatabase
 
 
 class WorkerModbus(threading.Thread):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, queue_worker_database: queue.Queue, queue_main_thread: queue.Queue,
+                 stop_workers_signal: threading.Event, event_generate: ttkbootstrap.Window.event_generate,
+                 lock: threading.Lock, trip_mode_flag_signal: threading.Event):
+        super().__init__(daemon=False)
+        self.queue_worker_database = queue_worker_database
+        self.queue_main_thread = queue_main_thread
+        self.stop_workers_signal = stop_workers_signal
+        self.event_generate = event_generate
+        self.lock = lock
+        self.modbus_query = ModbusQuery()
+        self.trip_mode_flag_signal = trip_mode_flag_signal
+
+    def run(self):
+        while not self.stop_workers_signal.is_set():
+            with self.lock:
+                telemetry: dict = self.modbus_query.read_and_format_telemetry_registers()
+            self.queue_worker_database.put(["telemetry", telemetry])  # note that it passes a list
+            self.queue_main_thread.put(telemetry)
+            self.event_generate("<<update_view>>")
+            if self.trip_mode_flag_signal.is_set():
+                time.sleep(1)
+            else:
+                if self.trip_mode_flag_signal.wait(timeout=15):
+                    continue
+        self.modbus_query.disconnect()
+        return 0
 
 
+class WorkerDatabase(threading.Thread):
+    def __init__(self, queue_worker_database: queue.Queue, stop_workers_signal: threading.Event,
+                 lock: threading.Lock):
+        super().__init__(daemon=False)
+        self.queue_worker_database = queue_worker_database
+        self.stop_workers_signal = stop_workers_signal
+        self.lock = lock
+        self.telemetry_database = TelemetryDatabase()
 
-worker_modbus = WorkerModbus()
-worker_modbus.start()
+    def run(self):
+        while not self.stop_workers_signal.is_set():
+            message = self.queue_worker_database.get()
+            with self.lock:
+                if message[0] == "telemetry":
+                    self.telemetry_database.insert_telemetry(message[1])
+                elif message[0] == "trip":
+                    self.telemetry_database.insert_trip(message[1])
+                elif message[0] == "end_trip":
+                    self.telemetry_database.end_of_trip()
+                else:
+                    raise ValueError("Not a valid queue message")
+        self.telemetry_database.close_connection()
+        return 0
