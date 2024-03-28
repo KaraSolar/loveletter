a34d8a05a553df9b2722b7c7d7a7519bf320a52e
danee593 <danee593@gmail.com>
2024-03-28 00:16:39 +0000

    minor changes to workers to improve functionality

--- controller/workers.py
+++ controller/workers.py
@@ -1,3 +1,9 @@
+"""
+TO DO:
+
+Consider changing the telemetry_data_bus to a @dataclass.
+"""
+
 import queue
 import threading
 import ttkbootstrap
@@ -7,32 +13,28 @@ from model.telemetry_database import TelemetryDatabase
 
 
 class WorkerModbus(threading.Thread):
-    def __init__(self, queue_worker_database: queue.Queue, queue_main_thread: queue.Queue,
-                 stop_workers_signal: threading.Event, event_generate: ttkbootstrap.Window.event_generate,
-                 trip_mode_flag_signal: threading.Event):
+    def __init__(self, queue_worker_database: queue.Queue, stop_workers_signal: threading.Event,
+                 event_generate: ttkbootstrap.Window.event_generate, trip_mode_flag_signal: threading.Event,
+                 lock: threading.Lock, telemetry_data_bus: dict):
         super().__init__(daemon=False)
         self.queue_worker_database = queue_worker_database
-        self.queue_main_thread = queue_main_thread
         self.stop_workers_signal = stop_workers_signal
         self.event_generate = event_generate
         self.modbus_query = ModbusQuery()
         self.trip_mode_flag_signal = trip_mode_flag_signal
+        self.lock = lock
+        self.telemetry_data_bus = telemetry_data_bus
 
-    def set_telemetry_dict_to_none(self) -> None:
-        """
-        This function encapsulates replacing every value in the telemetry dict to None.
-        :return: None
-        """
-        self.__telemetry = {key: None for key in self.__telemetry}
-
-    for index, key in enumerate(self.__telemetry):
-        self.__telemetry[key] = self.__register_values[index]
+    def write_telemetry_data_bus(self, telemetry_modbus):
+        with self.lock:
+            for index, key in enumerate(self.telemetry_data_bus):
+                self.telemetry_data_bus[key] = telemetry_modbus[index]
 
     def run(self):
         while not self.stop_workers_signal.is_set():
-            telemetry: dict = self.modbus_query.read_and_format_telemetry_registers()
-            self.queue_worker_database.put(["telemetry", telemetry])  # note that it passes a list
-            self.queue_main_thread.put(telemetry)
+            telemetry_modbus: list = self.modbus_query.read_and_format_telemetry_registers()
+            self.write_telemetry_data_bus(telemetry_modbus=telemetry_modbus)
+            self.queue_worker_database.put("telemetry")
             self.event_generate("<<update_view>>")
             if self.trip_mode_flag_signal.is_set():
                 time.sleep(1)
@@ -44,22 +46,24 @@ class WorkerModbus(threading.Thread):
 
 
 class WorkerDatabase(threading.Thread):
-    def __init__(self, queue_worker_database: queue.Queue, stop_workers_signal: threading.Event):
+    def __init__(self, queue_worker_database: queue.Queue, telemetry_data_bus: dict, lock: threading.Lock):
         super().__init__(daemon=False)
         self.queue_worker_database = queue_worker_database
-        self.stop_workers_signal = stop_workers_signal
+        self.telemetry_data_bus = telemetry_data_bus
+        self.lock = lock
         self.telemetry_database = TelemetryDatabase()
 
     def run(self):
-        while not self.stop_workers_signal.is_set():
+        while True:
             message = self.queue_worker_database.get()
-            if message[0] == "telemetry":
-                self.telemetry_database.insert_telemetry(message[1])
-            elif message[0] == "trip":
-                self.telemetry_database.insert_trip(message[1])
-            elif message[0] == "end_trip":
+            if message == "telemetry":
+                with self.lock:
+                    self.telemetry_database.insert_telemetry(self.telemetry_data_bus)
+            elif message == "end_trip":
                 self.telemetry_database.end_of_trip()
+            elif message == "destroy":
+                break
             else:
-                raise ValueError("Not a valid queue message")
+                self.telemetry_database.insert_trip(message)
         self.telemetry_database.close_connection()
         return 0
