4cd181866eff592e48bbebf5db8196b706fc49ba
jordi <cascodanny@gmail.com>
2025-01-24 09:48:19 -0500

    Algorithm Update
    Auto Tags detect added
    Beautiful log code added
    clines, repo_install, function added
    Array index bug corrected

--- install/install.sh
+++ install/install.sh
@@ -1,6 +1,11 @@
-INSTALL_VER="beta 1.0-4"
-DISPLAY_VER=$(echo $INSTALL_VER | sed "s|~alpha||g" | sed "s|~beta||g")
-	echo
+clear
+
+repo=https://github.com/KaraSolar/loveletter.git
+#repo=https://github.com/PPUC/ZeDMD.git
+tags=($(git ls-remote --tags $repo | awk -F'/' '{print $NF}'))
+
+log_stated_install(){
+	DISPLAY_VER=$(echo $INSTALL_VER | sed "s|~alpha||g" | sed "s|~beta||g")
 	echo ' __  __   ____   _______   ____   _____   ______   _____             __  __            ______  ____   _   _             _____ '
 	echo '|  \/  | / __ \ |__   __| / __ \ |  __ \ |  ____| / ____|     /\    |  \/  |    /\    |___  / / __ \ | \ | |    /\     / ____|'
 	echo '| \  / || |  | |   | |   | |  | || |__) || |__   | (___      /  \   | \  / |   /  \      / / | |  | ||  \| |   /  \   | (___  '
@@ -18,5 +23,64 @@ DISPLAY_VER=$(echo $INSTALL_VER | sed "s|~alpha||g" | sed "s|~beta||g")
 		echo "                                                  USE AT YOUR OWN RISK                      "
 	fi
 	echo "                                                       ${INSTALL_VER}                           "
+}
 
-echo "$(git ls-remote --tags https://github.com/KaraSolar/loveletter.git)"
+repo_install(){
+	dest_dir=$(basename -s .git "$repo")
+	if [[ -d $dest_dir ]]; then
+		echo "Directory '$dest_dir' already exists. Deleting it..."
+		rm -rf "$dest_dir"
+	fi
+	git clone --branch $INSTALL_VER --single-branch $repo --progress 2>&1 | sed '/You are in '\''detached HEAD'\''/,$d'
+}
+
+clines(){
+	local lines_to_clear=$1
+    for ((j=0; j<lines_to_clear; j++)); do
+        tput cuu1  # Mueve el cursor una línea hacia arriba
+        tput el    # Limpia la línea actual
+    done
+}
+
+echo "Repo Installing: $repo"
+echo -e "________________________Avalaible Tags_________________________\n"
+for i in "${!tags[@]}"; do
+    echo "$((i + 1)). ${tags[$i]}"
+done
+((i++))
+echo "_______________________________________________________________"
+
+if [[ ${#tags[@]} -eq 0 ]]; then
+    echo "No exist a valid TAG avalaible"
+else
+	echo -e '\nSelect a tag version to Install, or type "0" to defaul install'
+    read -p "->  Select betwen (1-$i): " response
+
+	clines 4
+
+	if [[ "$response" -gt "$i" ]]; then
+    	echo -e "    Tag selected: ($response) no exist"
+	elif [[ "$response" -eq 0 ]]; then
+		response=i
+		INSTALL_VER=${tags[response-1]}
+		log_stated_install
+		repo_install
+		#echo "You selected: ${tags[((response-1))]}"
+	else
+		INSTALL_VER=${tags[response-1]}
+		log_stated_install
+		repo_install
+		#echo "You selected: ${tags[((response-1))]}"
+	fi
+fi
+
+tput civis
+read -p "" input
+tput cnorm
+
+
+
+: '
+coment 
+multi
+lines'
