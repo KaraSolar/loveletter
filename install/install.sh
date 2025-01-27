3ff9cf9684a258846cf792baba470c2b7696e870
jordi <cascodanny@gmail.com>
2025-01-27 12:42:44 -0500

    Test

--- install/install.sh
+++ install/install.sh
@@ -1,8 +1,10 @@
 clear
-
-repo=https://github.com/KaraSolar/loveletter.git
 #repo=https://github.com/PPUC/ZeDMD.git
+repo=https://github.com/KaraSolar/loveletter.git
+repo_crons=https://github.com/KaraSolar/Rpi_Crons.git
+
 tags=($(git ls-remote --tags $repo | awk -F'/' '{print $NF}'))
+tag=$(git ls-remote --tags --sort="v:refname" $repo_crons | tail -n1 | awk -F'/' '{print $NF}')
 
 log_stated_install(){
 	DISPLAY_VER=$(echo $INSTALL_VER | sed "s|~alpha||g" | sed "s|~beta||g")
@@ -22,6 +24,40 @@ log_stated_install(){
 	fi
 	echo "                                             ${INSTALL_VER}                           "
 	echo ""
+}
+
+add_or_replace_variable() {
+    local variable="$1"  # The variable name
+    local value="$2"     # The value to set
+    local file="$3"      # The target file
+
+    if grep -q "^$variable=" "$file"; then
+        # Replace the existing variable line
+        sed -i "s|^$variable=.*|$variable=$value|" "$file"
+    else
+        # Append the variable to the end of the file
+        echo "$variable=$value" >> "$file"
+    fi
+}
+
+loveletter_service(){
+	echo -e "Installing Cron Jobs Services ...\n"
+	dest_dir=$(basename -s .git "$repo_crons")
+	if [[ -d $dest_dir ]]; then
+		echo "Directory '$dest_dir' already exists. Deleting it..."
+		rm -rf "$dest_dir"
+	fi
+	git clone --branch $tag --single-branch $repo_crons --progress 2>&1 | sed '/You are in '\''detached HEAD'\''/,$d'
+	sudo cp ~/Rpi_Crons/loveletter.service /etc/systemd/system/
+	auxdir=$(pwd)/loveletter
+	add_or_replace_variable "WorkingDirectory" "${auxdir}" "/etc/systemd/system/loveletter.service"
+	auxdir=$(pwd)/loveletter/start.sh
+	add_or_replace_variable "ExecStart" "${auxdir}" "/etc/systemd/system/loveletter.service"
+
+	sudo systemctl daemon-reload
+	sudo systemctl enable loveletter.service
+	sudo systemctl start loveletter.service
+
 }
 
 repo_install(){
@@ -52,6 +88,7 @@ repo_install(){
 	pip3 install six
 	pip3 install ttkbootstrap
 	pip3 install pymodbus==2.5.3
+
 	#rm -rf "install.sh"
 }
 
@@ -85,12 +122,16 @@ else
 		response=i
 		INSTALL_VER=${tags[response-1]}
 		log_stated_install
-		repo_install
+		loveletter_service
+		#repo_install
+
 		#echo "You selected: ${tags[((response-1))]}"
 	else
 		INSTALL_VER=${tags[response-1]}
 		log_stated_install
-		repo_install
+		loveletter_service
+		#repo_install
+		
 		#echo "You selected: ${tags[((response-1))]}"
 	fi
 fi
