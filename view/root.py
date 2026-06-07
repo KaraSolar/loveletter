fef5fd5049a92892b728442caa48c7f2cb65fd7b
danee593 <danee593@gmail.com>
2026-06-07 10:47:36 +0200

    restore the fullscreen

--- view/root.py
+++ view/root.py
@@ -5,7 +5,7 @@ class Root(ttk.Window):
         super().__init__(themename="cyborg",
                          title="Love Letter")
         self.geometry("800x500+0+0")
-        #self.after(4000, lambda: self.attributes("-fullscreen", True))
+        self.after(4000, lambda: self.attributes("-fullscreen", True))
         self.resizable(False,False)
         self.update_idletasks()
         # Title fonts
